7fd58893a28b5f23caca237d414ee4e60938acea
kjsheehy <kjsheehy@gmail.com>
2022-04-14 10:10:06 -0400

    handling of no number entered in number input added with click listener on Check! button

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct number!';
+console.log(document.querySelector('.message').textContent);
 
-//Why is creating an SSH key such an ordeal? Should I even do that vs just username/password???
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+console.log(document.querySelector('.number').textContent);
+
+console.log();
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number guessed';
+  }
+});
