7d6ecd2d9d4b9925049c75ffca0298a739a0f742
kjsheehy <kjsheehy@gmail.com>
2022-04-14 10:21:09 -0400

    Randomly generate secret number 1-20 and display (for now) for development purposes

--- script.js
+++ script.js
@@ -13,7 +13,11 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-document.querySelector('.check').addEventListener('click', function () {
+//Generate secret # 1-20
+let number = Math.ceil(Math.random() * 20);
+document.querySelector('.number').textContent = number;
+
+document.addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
